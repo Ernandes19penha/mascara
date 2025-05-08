c2f5c672cf7bb6054935e77d20596e9778a3831f
Ernandes penha <ernandes.santos.3251@gmail.com>
2025-05-08 17:35:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let inputCpf = document.querySelector("#cpf")
+
+inputCpf.addEventListener("keypress", () => {
+    let tamanhoCpf = inputCpf.value.length
+    if (tamanhoCpf === 3 || tamanhoCpf == 7) {
+        inputCpf.value += "."
+    } else if (tamanhoCpf == 11) {
+        inputCpf.value += "-"
+    }
+
+})
+
+
+let inputCnpj = document.querySelector("#cnpj")
+
+inputCnpj.addEventListener("keypress", ()=> {
+    let tamanhoCnpj = inputCnpj.value.length
+    if (tamanhoCnpj == 2 ||tamanhoCnpj == 6) {
+        inputCnpj.value += "."
+    } if (tamanhoCnpj == 10 ){
+        inputCnpj.value += "/"
+    }if (tamanhoCnpj == 15) {
+        inputCnpj.value += "-"
+    }
+})
